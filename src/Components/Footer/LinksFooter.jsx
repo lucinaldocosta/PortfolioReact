ba39ef6288979bd36a7b9531bd306357d6db35d8
Lucinaldo D Costa <dacostalucinaldo@gmail.com>
2023-09-06 09:36:09 -0300

    Solve Links Problems

--- src/Components/Footer/LinksFooter.jsx
+++ src/Components/Footer/LinksFooter.jsx
@@ -3,38 +3,34 @@ import "./FooterCSS/Footer.css";
 function LinksFooter() {
   return (
     <div>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/lucinaldo-costa"
-      ></a>
-      <img
-        className="links_footer"
-        src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png
+      <a target="_blank" href="https://www.linkedin.com/in/lucinaldo-costa">
+        <img
+          className="links_footer"
+          src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png
 "
-        alt="LC - Code Profile Linkedin"
-      />
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/lucinaldocosta"
-      ></a>
-      <img
-        className="links_footer"
-        src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png
+          alt="LC - Code Profile Linkedin"
+        />
+      </a>
+
+      <a target="_blank" href="https://github.com/lucinaldocosta">
+        <img
+          className="links_footer"
+          src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png
 "
-        alt="LC - Code Profile Github"
-      />
+          alt="LC - Code Profile Github"
+        />
+      </a>
+
       <a
         target="_blank"
-        rel="noreferrer"
         href="https://www.youtube.com/channel/UCkryn9jdUSAp3x9UI1RT_0A"
-      ></a>
-      <img
-        className="links_footer"
-        src="https://d33wubrfki0l68.cloudfront.net/493f1aa6da674c05bc606ba5b9e8092b881e8626/67c76/assets/png/yt-ico.png"
-        alt="LC - Code Profile Youtube"
-      />
+      >
+        <img
+          className="links_footer"
+          src="https://d33wubrfki0l68.cloudfront.net/493f1aa6da674c05bc606ba5b9e8092b881e8626/67c76/assets/png/yt-ico.png"
+          alt="LC - Code Profile Youtube"
+        />
+      </a>
     </div>
   );
 }
--- src/Components/Header/NavSocial.jsx
+++ src/Components/Header/NavSocial.jsx
@@ -12,34 +12,39 @@ function NavSocial({ NavPosition }) {
         target="_blank"
         rel="noreferrer"
         href="https://www.linkedin.com/in/lucinaldo-costa"
-      ></a>
-      <img
-        className="social_header"
-        src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png
+      >
+        <img
+          className="social_header"
+          src="https://d33wubrfki0l68.cloudfront.net/7f29579dde49e02480372aa49f7189c5536b0118/34b92/assets/png/linkedin-ico.png
 "
-        alt="LC - Code Profile Linkedin"
-      />
+          alt="LC - Code Profile Linkedin"
+        />
+      </a>
+
       <a
         target="_blank"
         rel="noreferrer"
         href="https://github.com/lucinaldocosta"
-      ></a>
-      <img
-        className="social_header"
-        src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png
+      >
+        <img
+          className="social_header"
+          src="https://d33wubrfki0l68.cloudfront.net/5557d5a11584d7201a38ee1a95200f57a4cc0f88/15085/assets/png/github-ico.png
 "
-        alt="LC - Code Profile Github"
-      />
+          alt="LC - Code Profile Github"
+        />
+      </a>
+
       <a
         target="_blank"
         rel="noreferrer"
         href="https://www.youtube.com/channel/UCkryn9jdUSAp3x9UI1RT_0A"
-      ></a>
-      <img
-        className="social_header"
-        src="https://d33wubrfki0l68.cloudfront.net/493f1aa6da674c05bc606ba5b9e8092b881e8626/67c76/assets/png/yt-ico.png"
-        alt="LC - Code Profile Youtube"
-      />
+      >
+        <img
+          className="social_header"
+          src="https://d33wubrfki0l68.cloudfront.net/493f1aa6da674c05bc606ba5b9e8092b881e8626/67c76/assets/png/yt-ico.png"
+          alt="LC - Code Profile Youtube"
+        />
+      </a>
     </article>
   );
 }
